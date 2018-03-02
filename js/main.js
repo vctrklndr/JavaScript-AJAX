1a035d790d0dba201238e7f10019bfae4a8b6db0
vctrklndr <victor@nasa.se>
2018-03-02 20:49:16 +0100

    Added JS

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
-function getBeers() {
-    fetch(`https://api.punkapi.com/v2/beers`)
-        .then((response) => response.json())
-        .then((beerData) => {
-            console.log(beerData);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+function getBeer() {
+    fetch(`https://api.punkapi.com/v2/beers?page=2&per_page=80`)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(beerData){
+        displayBeer(beerData);
+    })
+    .catch(function(error){
+        console.log(error);
+    })
 }
 
-getBeers();
+getBeer();
+
+function displayBeer(beerData){
+    const beerInfoElement = document.getElementById('beerInfo');
+
+    let beerInfo = `
+        <img src="${beerData[25].image_url}" alt="${beerData[25].name} " />
+        <h2> ${beerData[25].name} </h2>
+        <p class="tagline"> ${beerData[25].tagline} </p>
+        <p class="description"> ${beerData[25].description} </p>
+    `;
+    beerInfoElement.innerHTML = beerInfo;
+}
+
+
+// function displayBeer(beerData){
+//     const beerInfoElement = document.getElementById('beerInfo');
+
+//     let beerInfo = '';
+//     for (i = 0; i < beerData.length; i++) {
+//         console.log(beerData[i].name);
+//         beerInfo += `
+//         <div class="beerWrapper">
+//             <img src="${beerData[i].image_url}" alt="${beerData[i].name} " />
+//             <p> ${beerData[i].name} </p>
+//         </div>
+//     `}
+//     beerInfoElement.innerHTML = beerInfo;
+// }
